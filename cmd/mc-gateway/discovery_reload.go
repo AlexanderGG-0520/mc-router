3dd68bc7cfd490299cc4d36af6a014ea4b60e9b4
alexandergg-0520 <uket.panda.1st@gmail.com>
2026-06-01 20:28:24 +0900

    Build Kubernetes reload discovery report

--- cmd/mc-gateway/discovery_reload.go
+++ cmd/mc-gateway/discovery_reload.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/AlexanderGG-0520/mc-router/internal/config"
 	"github.com/AlexanderGG-0520/mc-router/internal/discovery"
 	k8sdiscovery "github.com/AlexanderGG-0520/mc-router/internal/discovery/kubernetes"
 )
@@ -19,6 +23,31 @@ func newReloadDiscoveryReport(result k8sdiscovery.Result) reloadDiscoveryReport 
 
 func (r reloadDiscoveryReport) routeProvider() discovery.RouteProvider {
 	return k8sdiscovery.NewSnapshotProviderFromResult(r.Result)
+}
+
+func buildReloadDiscoveryReport(ctx context.Context, cfg config.Config, deps discoveryStartupDeps) (reloadDiscoveryReport, error) {
+	deps = deps.withDefaults()
+	kubernetesConfig := cfg.Discovery.Kubernetes
+	namespace, err := deps.resolveNamespace(kubernetesConfig.Namespace, deps.namespaceResolver)
+	if err != nil {
+		return reloadDiscoveryReport{}, fmt.Errorf("resolve Kubernetes discovery namespace: %w", err)
+	}
+	restConfig, err := deps.inClusterConfig()
+	if err != nil {
+		return reloadDiscoveryReport{}, fmt.Errorf("create in-cluster Kubernetes config: %w", err)
+	}
+	lister, err := deps.newServiceLister(restConfig)
+	if err != nil {
+		return reloadDiscoveryReport{}, fmt.Errorf("create Kubernetes Service lister: %w", err)
+	}
+	services, err := lister.ListServices(ctx, namespace)
+	if err != nil {
+		return reloadDiscoveryReport{}, fmt.Errorf("list Kubernetes Services in resolved namespace: %w", err)
+	}
+	result := k8sdiscovery.BuildDiscoveredRoutes(services, k8sdiscovery.Options{
+		AnnotationPrefix: kubernetesConfig.AnnotationPrefix,
+	})
+	return newReloadDiscoveryReport(result), nil
 }
 
 func cloneKubernetesDiscoveryResult(result k8sdiscovery.Result) k8sdiscovery.Result {
