6146c2c8f0ebf5e215e7f2b77e56ac4c9a8ef66a
AlexanderGG <95066427+AlexanderGG-0520@users.noreply.github.com>
2026-08-11 17:46:26 +0900

    chore: update voicechat API to 2.6.20

--- companion/core/build.gradle.kts
+++ companion/core/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    compileOnly("de.maxhenkel.voicechat:voicechat-api:2.6.13")
+    compileOnly("de.maxhenkel.voicechat:voicechat-api:2.6.20")
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
